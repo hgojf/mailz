ba5ccb5e4203508dc34112d724c5158735fb7eeb
Henry Ford <fordhenry2299@gmail.com>
2024-08-15 20:55:51 +0000

    Correct paths

--- pathnames.h
+++ pathnames.h
@@ -2,8 +2,8 @@
 #define MAILZ_PATHNAMES_H
 #define PATH_DEV_NULL "/dev/null"
 #define PATH_LESS "/usr/bin/less"
-#define PATH_MAILDIR_READ "/usr/local/libexec/mailz/maildir-read"
-#define PATH_MAILDIR_READ_LETTER "/usr/local/libexec/mailz/maildir-read-letter"
+#define PATH_MAILDIR_READ "/usr/local/libexec/maildir-read"
+#define PATH_MAILDIR_READ_LETTER "/usr/local/libexec/maildir-read-letter"
 #define PATH_SENDMAIL "/usr/sbin/sendmail"
 #define PATH_TMPDIR "/tmp/mailz/"
 #endif /* MAILZ_PATHNAMES_H */
