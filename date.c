f6fdb25f8895f68069a781e0aba9f92f1df1768d
Henry Ford <fordhenry2299@gmail.com>
2024-09-02 01:44:10 +0000

    Reduce date validation to just avoid out of bounds accesses
    All dates are returned by localtime or constants in tests,
    so checking if they are valid per RFC 5322 is not super useful.

--- date.c
+++ date.c
@@ -13,8 +13,6 @@ static const char *weekdays[7] = {	"Sun", "Mon", "Tue", "Wed", "Thu",
 #define SEC_TO_HR(s) ((s) / (60 * 60))
 #define SEC_TO_MIN(s) ((s) / 60)
 
-static int date_valid(struct tm *, long, long);
-
 /*
  * Formats a date in RFC 5322 format.
  * The fields of 'tm' should be relative to
@@ -31,7 +29,11 @@ date_format(struct tm* tm, long off, char buf[static EMAIL_DATE_LEN])
 	hr = SEC_TO_HR(absoff);
 	mn = SEC_TO_MIN(absoff) - (hr * 60);
 
-	if (!date_valid(tm, hr, mn))
+	if (tm->tm_wday < 0 || tm->tm_wday > 6)
+		return 0;
+	if (tm->tm_wday < 0 || tm->tm_wday > 6)
+		return -1;
+	if (tm->tm_mon < 0 || tm->tm_mon > 11)
 		return -1;
 
 	n = snprintf(buf, EMAIL_DATE_LEN, "%s, %02d %s %02d %02d:%02d:%02d %c%02ld%02ld", 
@@ -74,45 +76,4 @@ date_parse(char *s)
 	date -= off;
 
 	return date;
-}
-
-/*
- * Checks if a given date and utc offset are valid per RFC 5322
- * section 3.3 paragraph 4
- */
-static int
-date_valid(struct tm *tm, long offh, long offm)
-{
-	if (tm->tm_wday < 0 || tm->tm_wday > 6)
-		return 0;
-
-	if (tm->tm_mon < 0 || tm->tm_mon > 11)
-		return 0;
-
-	if (tm->tm_mday < 1 || tm->tm_mday > 31)
-		return 0;
-
-	if (tm->tm_hour < 0 || tm->tm_hour > 23)
-		return 0;
-
-	if (tm->tm_min < 0 || tm->tm_min > 59)
-		return 0;
-
-	if (tm->tm_sec < 0 || tm->tm_sec > 60)
-		return 0;
-
-	/* 
-	 * According to RFC 5322, the last two digits of the timezone
-	 * offset (the minute offset) must be between 00 and 59. This 
-	 * implies that the first two digits (the hour offset) must be
-	 * no more than 2 digits. No other limits are imposed on the hour
-	 * portion of the timezone offset.
-	 */
-	if (offh < -99 || offh > 99)
-		return -1;
-
-	if (offm < 0 || offm > 59)
-		return 0;
-
-	return 1;
 }
