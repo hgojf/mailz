hgojf/mailz

commit d66a400ef96e53955d987d58a01cd85266ba9cf6

Simplify charset_getchar a byte. No functional change.
Similiar to previous encoding_getbyte change.

charset.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "charset.h"
@@ -16,31 +17,31 @@ charset_getchar(FILE *fp, struct charset *charset, char buf[static 4])
 {
 	int c;
 
-	if (charset->type == CHARSET_UTF8)
-		return utf8_getchar(&charset->val.utf8, &charset->encoding, fp, buf);
-
-	if ((c = encoding_getbyte(fp, &charset->encoding)) == ENCODING_ERR)
-		return -1;
-	if (c == ENCODING_EOF)
-		return 0;
-
 	switch (charset->type) {
 	case CHARSET_ASCII:
-		if (!isascii(c))
-			return -1;
+	case CHARSET_OTHER:
+		if ((c = fgetc(fp)) == EOF)
+			return 0;
+
+		if (!isascii(c)) {
+			if (charset->type == CHARSET_OTHER)
+				goto replace;
+			else
+				return -1;
+		}
+
 		if (!isprint(c) && !isspace(c))
 			goto replace;
-		break;
-	case CHARSET_OTHER:
-		if (!isascii(c) || (!isprint(c) && !isspace(c)))
-			goto replace;
-		break;
+
+		buf[0] = c;
+		return 1;
 	case CHARSET_UTF8:
-		assert(0);
+		return utf8_getchar(&charset->val.utf8, &charset->encoding, 
+			fp, buf);
 	}
 
-	buf[0] = c;
-	return 1;
+	/* invalid charset type */
+	abort();
 
 	replace:
 	/* utf8 replacement character */

commit 32eeda1a1fafff60920116cede3cabcdf0d6a39c

Dont skip content-transfer-encoding when charset is not UTF-8.
Additionally added a test to regress for this.
This was a regression introducted in commit 
d66a400ef96e53955d987d58a01cd85266ba9cf6

charset.c
@@ -20,7 +20,7 @@ charset_getchar(FILE *fp, struct charset *charset, char buf[static 4])
 	switch (charset->type) {
 	case CHARSET_ASCII:
 	case CHARSET_OTHER:
-		if ((c = fgetc(fp)) == EOF)
+		if ((c = encoding_getbyte(fp, &charset->encoding)) == ENCODING_EOF)
 			return 0;
 
 		if (!isascii(c)) {

regress.c
@@ -266,6 +266,7 @@ charset_test(void)
 		const char *encoding;
 	} charsets[] = {
 		{ "hello", "hello", "us-ascii", "7bit" },
+		{ "hel=6Co", "hello", "us-ascii", "quoted-printable" },
 		{ "\xf0\x9f\x90\xb7", "\xf0\x9f\x90\xb7", "UtF-8", "8bit" },
 		{ "=F0=9F=90=B7", "\xf0\x9f\x90\xb7", "utf-8", "quoted-printable" },
 	};