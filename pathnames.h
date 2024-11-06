3e4ebeeae5e62aec3eebdd6a8de28b1df8f92e96
Henry Ford <fordhenry2299@gmail.com>
2024-11-06 11:59:51 +0000

    Unstub sendmail use

--- pathnames.h
+++ pathnames.h
@@ -3,6 +3,6 @@
 #define PATH_DEV_NULL "/dev/null"
 #define PATH_MAILZ_CONTENT PREFIX "/libexec/mailz-content"
 #define PATH_LESS "/usr/bin/less"
-#define PATH_SENDMAIL "/bin/cat"
+#define PATH_SENDMAIL "/usr/sbin/sendmail"
 #define PATH_TMPDIR "/tmp/mailz.XXXXXX"
 #endif /* MAILZ_PATHNAMES_H */
