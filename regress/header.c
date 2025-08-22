091b4f3f296c25b5876f2c3290da8883de63722d
Henry Ford <fordhenry2299@gmail.com>
2025-08-22 13:07:49 +0000

    Test that nonprinting bytes are not allowed in message ids

--- regress/header.c
+++ regress/header.c
@@ -479,6 +479,7 @@ header_message_id_test(void)
 		{ "<uniq>", "uniq", 10, HEADER_OK },
 		{ "<uniq>", "uniq", 5, HEADER_OK },
 
+		{ "<hello\xFF>", NULL, 10, HEADER_INVALID },
 		{ "hello<uniq", NULL, 10, HEADER_INVALID },
 		{ "<uniq", NULL, 10, HEADER_INVALID },
 		{ "<uniq>", NULL, 4, HEADER_INVALID },
