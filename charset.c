e4af6e2536db28d9725c0d54f92eb01f4c5da994
Henry Ford <fordhenry2299@gmail.com>
2026-04-01 17:53:22 +0000

    Cleanup charset.c, similar to previous commit to encoding.c

--- charset.c
+++ charset.c
@@ -25,9 +25,9 @@
 
 #define nitems(a) (sizeof((a)) / sizeof(*(a)))
 
-static int charset_raw(struct encoding *, FILE *, int, char [static 4]);
-static int charset_iso_8859_1(struct encoding *, FILE *, char [static 4]);
-static int charset_utf8(struct encoding *, FILE *, char [static 4]);
+static int charset_getc_raw(struct encoding *, FILE *, int, char [static 4]);
+static int charset_getc_iso_8859_1(struct encoding *, FILE *, char [static 4]);
+static int charset_getc_utf8(struct encoding *, FILE *, char [static 4]);
 
 #define CTR_ASCII 0x1
 
@@ -60,26 +60,26 @@ charset_from_type(struct charset *c, enum charset_type type)
 }
 
 int
-charset_getc(struct charset *c, struct encoding *encoding, FILE *fp,
+charset_getc(struct charset *cs, struct encoding *encoding, FILE *fp,
 	     char buf[static 4])
 {
-	switch (c->type) {
+	switch (cs->type) {
 	case CHARSET_ASCII:
-		return charset_raw(encoding, fp, CTR_ASCII, buf);
+		return charset_getc_raw(encoding, fp, CTR_ASCII, buf);
 	case CHARSET_ISO_8859_1:
-		return charset_iso_8859_1(encoding, fp, buf);
+		return charset_getc_iso_8859_1(encoding, fp, buf);
 	case CHARSET_OTHER:
-		return charset_raw(encoding, fp, 0, buf);
+		return charset_getc_raw(encoding, fp, 0, buf);
 	case CHARSET_UTF8:
-		return charset_utf8(encoding, fp, buf);
+		return charset_getc_utf8(encoding, fp, buf);
 	default:
 		return -1;
 	}
 }
 
 static int
-charset_iso_8859_1(struct encoding *encoding, FILE *fp,
-		   char buf[static 4])
+charset_getc_iso_8859_1(struct encoding *encoding, FILE *fp,
+			char buf[static 4])
 {
 	mbstate_t mbs;
 	size_t n;
@@ -102,8 +102,8 @@ charset_iso_8859_1(struct encoding *encoding, FILE *fp,
 }
 
 static int
-charset_raw(struct encoding *e, FILE *fp, int flags,
-	    char buf [static 4])
+charset_getc_raw(struct encoding *e, FILE *fp, int flags,
+		 char buf [static 4])
 {
 	int ch;
 
@@ -125,7 +125,7 @@ charset_raw(struct encoding *e, FILE *fp, int flags,
 }
 
 static int
-charset_utf8(struct encoding *e, FILE *fp, char buf[static 4])
+charset_getc_utf8(struct encoding *e, FILE *fp, char buf[static 4])
 {
 	mbstate_t mbs;
 	int i;
